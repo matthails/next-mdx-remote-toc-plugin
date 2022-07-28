34ae1fed9a901aa355fb6450b4ecfd5207c0e95f
Matt <32686182+matthails@users.noreply.github.com>
2022-07-28 19:16:53 +0100

    first commit

---
+++ components/Layout.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Layout({ children }) {
+  return <div className="bg-slate-200">{children}</div>;
+}
+
+export default Layout;
---
+++ components/MyDiv.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function MyDiv() {
+  return <div className="w-4 h-4 bg-slate-200">MyDiv</div>;
+}
+
+export default MyDiv;
